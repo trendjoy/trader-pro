c986315c290a0758b0ebb0eaeb5535451084aa43
trendjoy <rmart24.rm@gmail.com>
2026-06-23 21:00:32 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,16 @@
+export const metadata = {
+  title: "Trader Pro",
+  description: "Trading Dashboard"
+}
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
